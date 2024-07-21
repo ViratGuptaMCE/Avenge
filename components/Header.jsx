ba948f99a141994efe628140dd92aa60defb445b
Virat Gupta <viratgupta1072@gmail.com>
2024-07-21 13:26:53 +0530

    Add Header Animation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,65 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import gsap from 'gsap';
 
 const Header = ({ className }) => {
   const [isOpen, setIsOpen] = useState(false);
-  // console.log(isOpen);
+  
+  
+  const t2 = gsap.timeline({ paused: true, reversed: true });
+  const navOpen = useRef();
+  const contact = useRef();
+  const social = useRef();
+  function animateHead() {
+    t2.to(navOpen.current,
+      {
+      y: "-100%",
+      duration: 0,
+      }
+    );
+    t2.to(contact.current, {
+      opacity: 0,
+      y: "-100%",
+      duration: 0
+    });
+    t2.to(social.current, {
+      opacity: 0,
+      y: "100%",
+      duration: 0,
+    });
+    t2.to(
+      navOpen.current,
+      {
+        y: "0%" ,
+        duration : 0.5
+      },      
+    )
+    t2.to(contact.current, {
+      opacity: 1,
+      y: "0%",
+      duration: 0.5,
+    });
+    t2.to(social.current, {
+      opacity: 1,
+      y: "0%",
+      duration: 0.5,
+    });
+      // .from(
+      //   contact.current,
+      //   0.5,
+      //   { opacity: 0, y: "-100%" },
+      //   { opacity: 1, y: "0%" }
+      // )
+      // .from(
+      //   social.current,
+      //   0.5,
+      //   { opacity: 0, y: "100%" },
+      //   { opacity: 1, y: "0%" }
+    // );
+    // console.log("\n isOpen is : ", isOpen);
+    !isOpen ? t2.play() : t2.reverse(1.5);
+  }
+
   return (
     <header className={className}>
       <nav className="w-full min-h-[80px] max-h-[150px] grid grid-cols-header grid-rows-2 px-[30px] py-[10px] ">
@@ -21,20 +77,31 @@ const Header = ({ className }) => {
           width={50}
           height={50}
           className=" align-middle row-start-1 col-start-3 row-end-2 cursor-pointer justify-self-end col-end-4"
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={(e) => {
+            e.preventDefault();
+            if(isOpen===false)  {
+              setIsOpen(!isOpen);
+              // console.log(isOpen);
+              animateHead();
+            } else {
+              animateHead();
+              setTimeout(() => setIsOpen(!isOpen),1400);
+            }
+          }}
         />
         <div
           className={`row-start-2 row-end-2 col-start-2 col-end-4 rounded-xl overflow-hidden ${
             !isOpen
               ? "hidden"
               : "flex justify-around items-center text-center backdrop-blur-[5px] bg-backColor-1"
-          } `}
+            } `}
+          ref={navOpen}
         >
-          <div className="contact">
+          <div className="contact" ref={contact}>
             <h3>Contact</h3>
             <p>www.getout#joke.comforyou</p>
           </div>
-          <div className="social">
+          <div className="social" ref={social}>
             <h3>Unsocial</h3>
             <p>Twitter</p>
           </div>
